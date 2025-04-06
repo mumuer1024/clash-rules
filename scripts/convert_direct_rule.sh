7d86ac95491cf6f1c06eb3310e0e641b494fffab
mumuer1024 <746429283@qq.com>
2025-04-06 04:37:48 -0400

    更新

--- scripts/convert_direct_rule.sh
+++ scripts/convert_direct_rule.sh
@@ -3,11 +3,11 @@
 mkdir -p rules
 
 # 下载 Shadowrocket 规则
-wget -O input_direct.module "https://raw.githubusercontent.com/GMOogway/shadowrocket-rules/master/sr_reject_list.module"
+wget -O sr_reject_list.module "https://raw.githubusercontent.com/GMOogway/shadowrocket-rules/master/sr_reject_list.module"
 
 # 转换为 OpenClash YAML 格式
 echo "payload:" > rules/reject_list.yaml
-grep -v '^#' input_direct.module | grep -v '^$' | sed 's/^/  - "/;s/$/"/' >> rules/reject_list.yaml
+grep -v '^#' sr_reject_list.module | grep -v '^$' | sed 's/^/  - "/;s/$/"/' >> rules/reject_list.yaml
 
 # 输出日志
 echo "转换完成！规则数量: $(grep -c '^  -' rules/reject_list.yaml)"
--- scripts/convert_proxy_rule.sh
+++ scripts/convert_proxy_rule.sh
@@ -3,11 +3,11 @@
 mkdir -p rules
 
 # 下载 Shadowrocket 规则
-wget -O input_direct.module "https://raw.githubusercontent.com/GMOogway/shadowrocket-rules/master/sr_proxy_list.module"
+wget -O sr_proxy_list.module "https://raw.githubusercontent.com/GMOogway/shadowrocket-rules/master/sr_proxy_list.module"
 
 # 转换为 OpenClash YAML 格式
 echo "payload:" > rules/proxy_list.yaml
-grep -v '^#' input_direct.module | grep -v '^$' | sed 's/^/  - "/;s/$/"/' >> rules/proxy_list.yaml
+grep -v '^#' sr_proxy_list.module | grep -v '^$' | sed 's/^/  - "/;s/$/"/' >> rules/proxy_list.yaml
 
 # 输出日志
 echo "转换完成！规则数量: $(grep -c '^  -' rules/proxy_list.yaml)"
